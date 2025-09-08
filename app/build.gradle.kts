e6b9b44d025240ced422d9623eef9a0301d38411
alamedahealthcare <ab@ab.com>
2025-09-08 22:01:47 +0300

    init Crypto Currency Compose Project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -56,4 +57,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
+    implementation(libs.bundles.koin)
+
+    implementation(libs.bundles.ktor)
+
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.debug)
+
+
 }
