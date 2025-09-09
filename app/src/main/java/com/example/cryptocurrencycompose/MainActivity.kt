bb261d7c593b55c0c3820e10b13d8089160d90e4
alamedahealthcare <abramibrahem22@gmail.com>
2025-09-09 09:48:08 +0300

    initial

--- app/src/main/java/com/example/cryptocurrencycompose/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrencycompose/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hellllo $name!",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
