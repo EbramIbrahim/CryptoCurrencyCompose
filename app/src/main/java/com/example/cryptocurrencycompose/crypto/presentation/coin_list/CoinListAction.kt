ecf6dbfb6a66a77b1b6c2a44b62bc1feedeb53e5
alamedahealthcare <abramibrahem22@gmail.com>
2025-09-12 16:15:15 +0300

    Crate CoinListViewModel and their actions

---
+++ app/src/main/java/com/example/cryptocurrencycompose/crypto/presentation/coin_list/CoinListAction.kt
@@ -0,0 +1,8 @@
+package com.example.cryptocurrencycompose.crypto.presentation.coin_list
+
+import com.example.cryptocurrencycompose.crypto.presentation.model.CoinUi
+
+sealed interface CoinListAction {
+    data class OnCoinClicked(val coinUi: CoinUi): CoinListAction
+    data object RefreshCoins: CoinListAction
+}
---
+++ app/src/main/java/com/example/cryptocurrencycompose/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.cryptocurrencycompose.crypto.presentation.coin_list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptocurrencycompose.core.domain.util.onError
+import com.example.cryptocurrencycompose.core.domain.util.onSuccess
+import com.example.cryptocurrencycompose.crypto.domain.data_source.CoinDataSource
+import com.example.cryptocurrencycompose.crypto.presentation.model.toCoinUi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CoinListViewModel(
+    private val coinDataSource: CoinDataSource
+) : ViewModel() {
+
+    private val _coinsState: MutableStateFlow<CoinListState> = MutableStateFlow(CoinListState())
+    val coinsState = _coinsState
+        .onStart { loadCoins() }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000L),
+            initialValue = CoinListState()
+        )
+
+    fun onAction(action: CoinListAction) {
+        when (action) {
+            is CoinListAction.OnCoinClicked -> TODO()
+            CoinListAction.RefreshCoins -> {
+                loadCoins()
+            }
+        }
+    }
+
+    private fun loadCoins() {
+        viewModelScope.launch {
+            _coinsState.update { it.copy(isLoading = true) }
+            coinDataSource.getCoins()
+                .onSuccess { coins ->
+                    _coinsState.update {
+                        it.copy(
+                            isLoading = false,
+                            coinList = coins.map { it.toCoinUi() })
+                    }
+                }
+                .onError { error ->
+                    _coinsState.update {
+                        it.copy(
+                            isLoading = false,
+                        )
+                    }
+                }
+        }
+    }
+
+}
