7ca30f124ea70612a12f18f7d53426a3cad705d3
alamedahealthcare <abramibrahem22@gmail.com>
2025-09-16 09:52:51 +0300

    add coin icons resources

--- app/src/main/java/com/example/cryptocurrencycompose/core/presentation/util/CoinCodeToResource.kt
+++ app/src/main/java/com/example/cryptocurrencycompose/core/presentation/util/CoinCodeToResource.kt
@@ -5,7 +5,11 @@ import com.example.cryptocurrencycompose.R
 // TODO() add all coins to this list
 fun coinCodeToResource(code: String): Int {
     return when(code.uppercase()) {
+        "BTC" -> R.drawable.btc
         "INCH" -> R.drawable._inch
+        "ST" -> R.drawable._st
+        "XBTC" -> R.drawable._xbtc
+        "AAVE" -> R.drawable.aave
         else -> R.drawable._inch
     }
 }
